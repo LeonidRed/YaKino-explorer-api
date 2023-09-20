977a523e3650299e39a083047c68f12629f9a802
LeonidRed <nikulin.l.m@gmail.com>
2023-09-20 23:15:02 +0300

    добавлен сервер фронта в cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  // 'https://yakino.nomoreparties.co',
-  // 'http://yakino.nomoreparties.co',
+  'https://yakino.nomoredomainsrocks.co',
+  'http://yakino.nomoredomainsrocks.co',
   // 'localhost:3000',
   // 'http://localhost:3000',
   'http://localhost:3002',
